7da995a0d987cde2153287407413e37b80584a62
Kirill Malskiy <sabbat@mail.ru>
2023-09-10 10:58:31 +0300

    установил и подключил body-parser

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
+app.use(bodyParser.json());
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
-})
+});
 
