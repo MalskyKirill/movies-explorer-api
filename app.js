6fd10f4d2a6b86e5b4e4f29d7dd42310133b2540
Kirill Malskiy <sabbat@mail.ru>
2023-09-10 17:49:15 +0300

    установил монгусс и подключился к бд

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+// подключение к бд
+mongoose
+  .connect('mongodb://127.0.0.1:27017/moviesdb')
+  .then(() => {
+    console.log('подключение к базе данных проекта movies');
+  });
 
 const app = express();
 
@@ -7,7 +15,7 @@ const { PORT = 3000 } = process.env;
 
 app.use(bodyParser.json());
 
+//
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
-
