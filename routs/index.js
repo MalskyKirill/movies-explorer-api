b274b3c6007228036096e911e0e9a68e69406450
Kirill Malskiy <sabbat@mail.ru>
2023-09-13 18:17:39 +0300

    создал мидлвару auth и защитил api авторизацией

--- routs/index.js
+++ routs/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const authRouter = require('./auth');
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
+const auth = require('../midlewares/auth');
 
 const NotFoundError = require('../utils/errors/NotFoundError');
 
+router.use('/', authRouter);
+router.use(auth);
 router.use('/users', usersRouter);
 router.use('/movies', moviesRouter);
 router.use('*', () => {
